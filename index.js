75dc8f47ae559b0ff9d810219bfd2e43c989d24f
Rei Orozco <rfoc15@gmail.com>
2022-03-28 13:59:13 -0500

    orderIndex

--- index.js
+++ index.js
@@ -3,15 +3,14 @@ const app = express();
 const config = require("./config/config");
 const logger = require("winston");
 
+if (config.NODE_ENV === "production") {
+  require("./startup/prod")(app);
+}
 require("./startup/logging")();
 require("./startup/routes")(app);
 require("./startup/db")();
 require("./startup/config")();
 require("./startup/validation")();
-
-if (config.NODE_ENV === "production") {
-  require("./startup/prod")(app);
-}
 
 const server = app.listen(process.env.PORT, process.env.HOST, () =>
   logger.info(
