656a8107eea3ed633ec9f99d5590076e558f1890
Rei Orozco <rfoc15@gmail.com>
2022-03-28 11:42:48 -0500

    Cors origin

--- startup/prod.js
+++ startup/prod.js
@@ -5,5 +5,13 @@ const cors = require("cors");
 module.exports = function (app) {
   app.use(helmet());
   app.use(compression());
-  app.use(cors({ credentials: true }));
+  app.use(
+    cors({
+      origin: [
+        "https://vidly-app-reiorozco.herokuapp.com/",
+        "http://localhost:*",
+      ],
+      credentials: true,
+    })
+  );
 };
